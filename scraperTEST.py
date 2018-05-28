0ae0b34100d60b92ff4fff144ba77c825f66787f
mikeynon <mikeynon@gmail.com>
2018-05-28 16:55:37 -0700

    tabs and spaces

--- scraperTEST.py
+++ scraperTEST.py
@@ -1,15 +1,12 @@
-import datetime
-import schedule
-import requests
-import random
-from bs4 import BeautifulSoup
-
+import schedule, time, requests, random, datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 usernameStr = 'cliefer'
 passwordStr = 'Stewie5!'
+
 def job():
     print("Starting Scheduler...")
     browser = webdriver.Chrome()
