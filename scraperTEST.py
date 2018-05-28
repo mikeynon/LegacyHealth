e9ff28d224c9804bc5817aa8a13889f135963d57
mikeynon <mikeynon@gmail.com>
2018-05-28 15:56:30 -0700

    moving imports

--- scraperTEST.py
+++ scraperTEST.py
@@ -1,19 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-usernameStr = 'putYourUsernameHere'
-passwordStr = 'putYourPasswordHere'import datetime
+import datetime
 import schedule
 import requests
 import random
 from bs4 import BeautifulSoup
 
-today = datetime.datetime.now().strftime("%m/%d/%y")
-shift = "0700-12"
-soup = requests.get(url)
-data = BeautifulSoup(soup, 'html.parser')
-if TypeError:
-    print()
-else:
-    print(data)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+usernameStr = 'cliefer'
+passwordStr = 'Stewie5!'
+browser = webdriver.Chrome()
+browser.get(('https://mytimeremote.lhs.org/APIHC/TASS/WebPortal/APIHealthcare/Login.aspx'))
+# fill in username and hit the next button
+username = browser.find_element_by_id('Email')
+username.send_keys(usernameStr)
+nextButton = browser.find_element_by_id('next')
+nextButton.click()
+
