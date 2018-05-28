b10882bd738d991e62efe81be786b3aa1ad356bc
mikeynon <mikeynon@gmail.com>
2018-05-28 14:47:52 -0700

    added schedule and bs4

--- bsoup.py
+++ bsoup.py
@@ -1,33 +1,37 @@
 import datetime
+import schedule
 import random
 from bs4 import BeautifulSoup
 
 today = datetime.datetime.now().strftime("%m/%d/%y")
 shift = "0700-12"
+soup = BeautifulSoup(url, 'html.parser')
 
+def job():
+    print("I'm doing your schedule for you...")
 ##LOGIN
-url = "https://mytimeremote.lhs.org/APIHC/TASS/WebPortal/APIHealthcare/Login.aspx"
-name_id = "formContentPlaceholder$_userNameField"
+    url = "https://mytimeremote.lhs.org/APIHC/TASS/WebPortal/APIHealthcare/Login.aspx"
+    name_id = "formContentPlaceholder$_userNameField"
 ##cleifer
-pw_id = "formContentPlaceholder$_passwordField"
+    pw_id = "formContentPlaceholder$_passwordField"
 ##Stewie5!
-sign_in = "formContentPlaceholder_loginApiButton"
+    sign_in = "formContentPlaceholder_loginApiButton"
 ##signin click
 
 ##SECTIONPICK
-a_translate = "sections.employee"
+    a_translate = "sections.employee"
 ##employee tab click
 
 ##SCHEDULESELF
-sched_id = "formContentPlaceholder_selfScheduleApiButton"
+    sched_id = "formContentPlaceholder_selfScheduleApiButton"
 ## schedule click
 
 ##DATEPICK
-table_id = "formContentPlaceholder_myScheduleTable"
-img_id = "formContentPlaceholder_activityList_3_rightImage"
+    table_id = "formContentPlaceholder_myScheduleTable"
+    img_id = "formContentPlaceholder_activityList_3_rightImage"
 ##click
-div_class = " dropDown dropDownBox"
-day = div_class.select("div div:nth-of-type(2)")
+    div_class = " dropDown dropDownBox"
+    day = div_class.select("div div:nth-of-type(2)")
 ##second child onmousedown= "setValue(this, event)"
 
 
@@ -35,8 +39,13 @@ day = div_class.select("div div:nth-of-type(2)")
 ##THREE SHIFTS PER WEEK
 ##THREE WEEKENDS per SIX WEEK BLOCK
 ##randomly 3 out of every 7 (excluding 3+7's), min 3 weekends (multiples 7, +=1) <= 78
-index_list = [range(0,43)]
-weekends = [7,14,21,28,35,42]
+    index_list = [range(0,43)]
+    weekends = [7,14,21,28,35,42]
 
-save_sched = "formContentPlaceholder_saveApiButton"
+    save_sched = "formContentPlaceholder_saveApiButton"
 ##click
+
+schedule.every().sunday.at("00:01").do(job)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
